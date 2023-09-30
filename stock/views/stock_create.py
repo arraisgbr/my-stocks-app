562eb813a3408c998db7be65dbc440b7c63152ce
Gabriel Arrais <arrais.gbr@usp.br>
2023-09-30 11:03:41 -0300

    fix stock form create automatically getting the user logged-in information

--- stock/views/stock_create.py
+++ stock/views/stock_create.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.views.generic.edit import FormView
 from stock.forms import StockForm
 from django.urls import reverse_lazy
@@ -9,5 +10,6 @@ class StockCreateView(FormView):
     success_url = reverse_lazy('stock:stock_list')
 
     def form_valid(self, form):
+        form.instance.user = self.request.user
         form.save()
         return super().form_valid(form)
